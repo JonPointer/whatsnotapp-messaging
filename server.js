4d17790359acabf12bd5392e5f739f2a30a87439
jamesosull16 <jamesosull16@gmail.com>
2021-01-12 19:55:48 -0700

    adding the sync function

--- server.js
+++ server.js
@@ -4,23 +4,29 @@
 
 // Dependencies
 // =============================================================
-const express = require('express');
+const express = require("express");
 
 // Sets up the Express App
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+db.sequalize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log("Listening on port %s", PORT);
+  });
+});
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory to be served
-app.use(express.static('app/public'));
+app.use(express.static("app/public"));
 
 // Routes
 // =============================================================
-require('./app/routes/api-routes.js')(app);
+require("./app/routes/api-routes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
