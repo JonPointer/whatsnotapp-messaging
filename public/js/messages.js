179b6c0df29f099d4bf4398fce40750222a77388
jamesosull16 <jamesosull16@gmail.com>
2021-01-14 19:47:00 -0700

    cleaner & working server side code

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,9 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Message = sequelize.define("Message", {
-    //need to decide on what data the message will include
-    //the user who sent it
-    //the body of the message
-    //time/date?
+    username: DataTypes.STRING,
+    message: DataTypes.STRING
   });
   return Message;
 };
