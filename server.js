b1b257238d4b0b5b9f5e3a009856b6d2f41797e5
jamesosull16 <jamesosull16@gmail.com>
2021-01-14 21:21:33 -0700

    socketio route functionality

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const exphbs = require("express-handlebars");
 const db = require("./models");
 const path = require("path");
 
-const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +19,12 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.use(express.static(path.join(__dirname, "/")));
 app.set("view engine", "handlebars");
 
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+  http.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 });
