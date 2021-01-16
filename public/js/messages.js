31576abb724f831053ebb9d752137892db5221c6
jamesosull16 <jamesosull16@gmail.com>
2021-01-16 13:50:05 -0700

    js and routes for searching

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,5 +1,3 @@
-
-
 const socket = io();
 
 const messages = document.getElementById("messages");
@@ -9,34 +7,74 @@ const input = document.getElementById("input");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value) {
-    socket.emit("chat message", (username.value + ": " + input.value));
-    // Message has been sent to the back end. Now need to call the 
+    socket.emit("chat message", username.value + ": " + input.value);
+    // Message has been sent to the back end. Now need to call the
     // API to add to the database
     // First need to build an object of the message and user
     let newMsg = {
       username: username.value,
       message: input.value,
-    }
+    };
     // Now call the API with the newMsg object
-    fetch('/api/messages', {
-      method: 'POST',
+    fetch("/api/messages", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(newMsg),
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log('Success in adding msg:', data);
+        console.log("Success in adding msg:", data);
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error("Error:", error);
       });
     // Now need to reset the input field
     input.value = "";
   }
 });
 
+const viewAllBtn = document.getElementById("viewAllBtn");
+if (viewAllBtn) {
+  viewAllBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("clicked");
+    fetch("/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success in retrieving all messages", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+}
+
+const userBtn = document.getElementById("userBtn");
+if (userBtn) {
+  userBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("clicked");
+    fetch(`/api/messages/${username}`, {
+      method: "GET",
+      "Content-Type": "application/json",
+    })
+      .then((response) => response.json)
+      .then((data) => {
+        console.log("Search username messages", data);
+        const { username } = data;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+}
 // When a message is received from the back end, write it to the screen
 socket.on("chat message", (msg) => {
   const item = document.createElement("li");
@@ -44,5 +82,3 @@ socket.on("chat message", (msg) => {
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
 });
-
-
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,7 @@
+const { response } = require("express");
 const db = require("../models");
 
 module.exports = (app) => {
-
   //POST route for saving new message to db
   app.post("/api/messages", (req, res) => {
     db.Message.create({
@@ -16,10 +16,34 @@ module.exports = (app) => {
       // Construct a new array from the username and body values of dbMessage
       let passArray = [];
       for (i = 0; i < dbMessage.length; i++) {
-        passArray[i] = { username: dbMessage[i].username, body: dbMessage[i].body };
+        passArray[i] = {
+          username: dbMessage[i].username,
+          body: dbMessage[i].body,
+        };
       }
       // Now pass this array to index to be rendered
       res.render("index", { values: passArray });
     });
+  });
+
+  app.get("/api/messages/:username", (req, res) => {
+    if (req.params.username) {
+      Message.findAll({
+        where: {
+          username: req.params.username,
+        },
+      }).then((dbMessage) => {
+        // Construct a new array from the username and body values of dbMessage
+        let passArray = [];
+        for (i = 0; i < dbMessage.length; i++) {
+          passArray[i] = {
+            username: dbMessage[i].username,
+            body: dbMessage[i].body,
+          };
+        }
+        // Now pass this array to index to be rendered
+        res.render("index", { values: passArray });
+      });
+    }
   });
 };
