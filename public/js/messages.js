b03bef768b63163f62dc5c76af6d09d52d3720ac
Jon Pointer <jonpointer@msn.com>
2021-01-19 22:21:40 -0700

    Changed input form setup and JS logic to support. Added auto scroll to bottom of messages

--- public/js/messages.js
+++ public/js/messages.js
@@ -8,6 +8,8 @@ const userForm = document.getElementById("userForm");
 const input = document.getElementById("input");
 const refresh = document.getElementById("refresh-button");
 
+messages.scrollTop = messages.scrollHeight;
+
 refresh.onclick = () => {
   // location.reload("/");
   location.href = "/";
@@ -35,6 +37,7 @@ userDisplay = (objButton) => {
   //   });
 };
 
+var msgFromUser = "";
 
 var userID = localStorage.getItem("userID");
 var usersName = localStorage.getItem("usersName");
@@ -51,60 +54,117 @@ if (usersName === null) {
 
 // let userID = 0;
 
-userForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (username.value) {
-    // Call API for a new user
-    fetch('/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name: username.value }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        userID = data.id;
-        // Now need to store the usersName and userID to local storage
-        localStorage.setItem("usersName", username.value);
-        localStorage.setItem("userID", userID);
-        console.log('Success in adding user:', data);
-        console.log(userID);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }
-});
+// userForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   if (username.value) {
+//     // Call API for a new user
+//     fetch('/api/users', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ name: username.value }),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         userID = data.id;
+//         // Now need to store the usersName and userID to local storage
+//         localStorage.setItem("usersName", username.value);
+//         localStorage.setItem("userID", userID);
+//         console.log('Success in adding user:', data);
+//         console.log(userID);
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   }
+// });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (input.value) {
-    socket.emit("chat message", (username.value + ": " + input.value));
-    // Message has been sent to the back end. Now need to call the 
-    // API to add to the database
-    // First need to build an object of the message and user
-    let newMsg = {
-      message: input.value,
-      UserId: userID,
-    }
-    // Now call the API with the newMsg object
-    fetch('/api/messages', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newMsg),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Success in adding msg:', data);
+  if ((input.value) && (username.value)) {
+
+    console.log(userID);
+    console.log(usersName.trim() + " : " + username.value.trim());
+
+    // if ((userID === null) && (username.value) && (input.value)) {
+    if ((userID === null) || (usersName.trim() != username.value.trim())) {
+
+      msgFromUser = input.value;
+
+      fetch('/api/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: username.value }),
       })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          userID = data.id;
+          usersName = username.value;
+          // Now need to store the usersName and userID to local storage
+          localStorage.setItem("usersName", username.value);
+          localStorage.setItem("userID", userID);
+          console.log('Success in adding user:', data);
+          console.log(userID);
+
+          socket.emit("chat message", (username.value + ": " + msgFromUser));
+          // Message has been sent to the back end. Now need to call the 
+          // API to add to the database
+          // First need to build an object of the message and user
+          let newMsg = {
+            message: msgFromUser,
+            UserId: userID,
+          }
+          // Now call the API with the newMsg object
+          fetch('/api/messages', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newMsg),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log('Success in adding msg:', data);
+            })
+            .catch((error) => {
+              console.error('Error:', error);
+            });
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    } else {
+      socket.emit("chat message", (username.value + ": " + input.value));
+      // Message has been sent to the back end. Now need to call the 
+      // API to add to the database
+      // First need to build an object of the message and user
+      let newMsg = {
+        message: input.value,
+        UserId: userID,
+      }
+      // Now call the API with the newMsg object
+      fetch('/api/messages', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMsg),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Success in adding msg:', data);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
+
     // Now need to reset the input field
     input.value = "";
+    // username.value = usersName;
   }
 });
 
@@ -113,7 +173,8 @@ socket.on("chat message", (msg) => {
   const item = document.createElement("li");
   item.textContent = msg;
   messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+  // window.scrollTo(0, document.body.scrollHeight);
+  messages.scrollTop = messages.scrollHeight;
 });
 
 
